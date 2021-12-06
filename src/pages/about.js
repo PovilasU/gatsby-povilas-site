43af2c5934163decf9fcabf8fcd0e0d4e0b0af36
PovilasU <povilas.urbonas85@gmail.com>
2021-12-06 20:24:31 +0100

    added link component to increase speed

--- src/pages/about.js
+++ src/pages/about.js
@@ -4,7 +4,7 @@ const AboutPage = () => {
   return (
     <article>
       <h1>About me</h1>
-      <p>I currenty work as a full-time web developer in a company Allente</p>
+      <p>I currenty work full-time in a company Allente</p>
       <p>For this project I use Gatsby framework.</p>
       <p>
         Gatsby is an open-source framework that combines functionality from
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,15 @@
 import React from "react"
+import { Link } from "gatsby"
 
 const IndexPage = () => {
   return (
     <div>
       <h1>Hello.</h1>
       <h2>I'm Povilas, full-stact developer.</h2>
+      <p>
+        The best way to reach me is via &nbsp;
+        <Link to="/contact">Contact me</Link>.
+      </p>
     </div>
   )
 }
