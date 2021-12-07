bd536e1c250a992b10445c1081510c6b07cc26a9
PovilasU <povilas.urbonas85@gmail.com>
2021-12-07 12:59:43 +0100

    Added graphql, fetching data using graphql queries

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,5 +6,9 @@
 
 module.exports = {
   /* Your site config here */
+  siteMetadata: {
+    title: "Full-Stack Website!",
+    author: "Povilas Urbonas",
+  },
   plugins: ["gatsby-plugin-sass"],
 }
--- src/components/footer.js
+++ src/components/footer.js
@@ -1,9 +1,19 @@
 import React from "react"
+import { graphql, useStaticQuery } from "gatsby"
 
 const Footer = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      site {
+        siteMetadata {
+          author
+        }
+      }
+    }
+  `)
   return (
     <footer>
-      <p>Created by Povilas, &copy; 2021</p>
+      <p>Created by {data.site.siteMetadata.author}, &copy; 2021</p>
     </footer>
   )
 }
