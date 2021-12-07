60b65c9f16180533165bb0b0c102f2d77d4955ed
PovilasU <povilas.urbonas85@gmail.com>
2021-12-07 16:30:43 +0100

    fetching posts with graphql

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,10 +1,36 @@
 import React from "react"
 import Layout from "../components/layout"
+import { graphql, useStaticQuery } from "gatsby"
 
 const BlogPage = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      allMarkdownRemark {
+        edges {
+          node {
+            frontmatter {
+              title
+              date
+            }
+          }
+        }
+      }
+    }
+  `)
+
   return (
     <Layout>
       <h1>Blog</h1>
+      <ol>
+        {data.allMarkdownRemark.edges.map((edge, i) => {
+          return (
+            <li key="i">
+              <h2>{edge.node.frontmatter.title}</h2>
+              <p>{edge.node.frontmatter.date}</p>
+            </li>
+          )
+        })}
+      </ol>
       <p>Posts wll show up here later on.</p>
     </Layout>
   )
