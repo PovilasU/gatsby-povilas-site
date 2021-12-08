fe7d01e6fbf30c23a0ba55ed74f6ebffeebd6548
PovilasU <povilas.urbonas85@gmail.com>
2021-12-09 00:09:07 +0100

    imported graphqp to all js files

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,7 @@ import React from "react"
 import { Link } from "gatsby"
 import Layout from "../components/layout"
 import Head from "../components/head"
+import { graphql } from "gatsby"
 
 const IndexPage = () => {
   return (
