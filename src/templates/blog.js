0f8790cdad7dd4d5bc801f4a6c591d5c53db714b
PovilasU <povilas.urbonas85@gmail.com>
2021-12-08 22:45:36 +0100

    generating images from contentful

--- src/templates/blog.js
+++ src/templates/blog.js
@@ -1,5 +1,7 @@
 import React from "react"
 import { documentToReactComponents } from "@contentful/rich-text-react-renderer"
+import { BLOCKS, MARKS } from "@contentful/rich-text-types"
+import { renderRichText } from "gatsby-source-contentful/rich-text"
 import { graphql } from "gatsby"
 import Layout from "../components/layout"
 
@@ -10,12 +12,52 @@ export const query = graphql`
       publishedDate(formatString: "MMM Do, YYYY")
       body {
         raw
+        references {
+          description
+          file {
+            url
+          }
+        }
       }
     }
   }
 `
 
 const Blog = props => {
+  const options1 = {
+    renderNode: {
+      "embedded-asset-block": node => {
+        console.log(node)
+        // const alt = node.data.target.fields.title["en-US"]
+        // const url = node.data.target.fields.file["en-US"].url
+        // return <img alt={alt} src={url} />
+      },
+    },
+  }
+
+  const options = {
+    renderNode: {
+      [BLOCKS.EMBEDDED_ASSET]: node => {
+        // console.log(node)
+        const alt = node.data.target.title
+        const url = node.data.target.fixed.src
+        console.log(url)
+        // return <img alt={alt} src={url} />
+      },
+    },
+  }
+
+  const references = props.data.contentfulBlogPost.body.references
+
+  const images = references.map((ref, i) => {
+    return (
+      <figure key={i}>
+        <img src={ref.file.url} alt={ref.description} />
+        <figcaption>{ref.description}</figcaption>
+      </figure>
+    )
+  })
+
   return (
     <Layout>
       <h1>{props.data.contentfulBlogPost.title}</h1>
@@ -23,6 +65,8 @@ const Blog = props => {
       {documentToReactComponents(
         JSON.parse(props.data.contentfulBlogPost.body.raw)
       )}
+
+      {images}
     </Layout>
   )
 }
